4e47253b1efd6b8a363d73e041e760a2e2b38d9f
Ymir <ymirthorleifsson@gmail.com>
2020-03-15 19:05:50 +0000

    newline char test ran green

--- project.py
+++ project.py
@@ -2,7 +2,7 @@ def add(numbers):
     if not numbers:
         return 0
         
-    list_numbers = numbers.split(",")
+    list_numbers = numbers.replace("\n", ",").split(",")
     ret_val = sum([int(x) for x in list_numbers])
 
     return ret_val
--- test_project.py
+++ test_project.py
@@ -12,3 +12,6 @@ def test_add_two_digits():
 def test_add_mult_digits():
     assert add("1,2,3,4,5") == 15
     assert add("10,2,5,22,1,1") == 41
+
+def test_newline_split():
+    assert add("1\n2,3") == 6
